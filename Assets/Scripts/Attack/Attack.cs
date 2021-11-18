97e7196bd2dd0e835ddfeba6edc91478de5bef08
creoii <33011337+creoii@users.noreply.github.com>
2021-11-18 13:18:45 -0500

    add angleoffset to attacks SPIRALS

--- Assets/Scripts/Attack/Attack.cs
+++ Assets/Scripts/Attack/Attack.cs
@@ -9,23 +9,25 @@ public class Attack
     public float lifetime;
     public float speed;
     public int projectileCount = 1;
+    public int angleOffset = 0;
     public int angleGap = 0;
     public bool onMouse = false;
 
     public MultiplierData acceleration = null;
 
-    public Attack(string sprite, float lifetime, float speed, int projectileCount, int angleGap, bool onMouse, MultiplierData acceleration)
+    public Attack(string sprite, float lifetime, float speed, int projectileCount, int angleOffset, int angleGap, bool onMouse, MultiplierData acceleration)
     {
         this.sprite = sprite;
         this.lifetime = lifetime;
         this.speed = speed;
         this.projectileCount = projectileCount;
+        this.angleOffset = angleOffset;
         this.angleGap = angleGap;
         this.onMouse = onMouse;
         this.acceleration = acceleration;
     }
 
-    public Attack(string sprite, float lifetime, float speed) : this(sprite, lifetime, speed, 1, 0, false, null)
+    public Attack(string sprite, float lifetime, float speed) : this(sprite, lifetime, speed, 1, 0, 0, false, null)
     {
 
     }
@@ -37,6 +39,7 @@ public class Attack
             two.lifetime == 0 ? one.lifetime : two.lifetime,
             two.speed == 0 ? one.speed : two.speed,
             two.projectileCount == 0 ? one.projectileCount : two.projectileCount,
+            two.angleOffset == 0 ? one.angleOffset : two.angleOffset,
             two.angleGap == 0 ? one.angleGap : two.angleGap,
             two.onMouse,
             MultiplierData.Override(one.acceleration, two.acceleration)
@@ -48,7 +51,7 @@ public class Attack
         if (projectileCount > 0)
         {
             Vector3 position = onMouse ? MouseUtil.GetMouseWorldPos() : character.transform.position;
-            float angle = MouseUtil.GetMouseAngle(position);
+            float angle = MouseUtil.GetMouseAngle(position) + angleOffset;
             if (angle < 0) angle += 360f;
             if (projectileCount > 1) angle -= angleGap * (((projectileCount - 1f) / 2f) + 1);
 
